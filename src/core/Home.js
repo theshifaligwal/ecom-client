3bd0aa6322f0db8efe73ea4533a99d42123ce8aa
theshifaligwal <gwal.shifali2223@gmail.com>
2021-08-12 17:49:02 +0530

    Inject backend into frontend env

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "../styles.css";
+import {API} from "../backend"
 
 export default function Home() {
+  console.log("API IS", API);
   return (
     <div>
-      <h1 className="text-white">Hello front end</h1>
+      <h1 className="text-white">Hello front end {API}</h1>
     </div>
   );
 }
