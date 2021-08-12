2a658cd3f417b0d4786d6575f80edafa1887a307
theshifaligwal <gwal.shifali2223@gmail.com>
2021-08-12 17:22:16 +0530

    Add css in react app

---
+++ src/core/Home.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-white">Hello front end</h1>
+    </div>
+  );
+}
