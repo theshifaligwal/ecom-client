bbc488fef62e0e9062364962f6418317f07338ab
theshifaligwal <gwal.shifali2223@gmail.com>
2021-08-13 11:46:09 +0530

    Create route for signin and signout

--- src/core/Home.js
+++ src/core/Home.js
@@ -6,7 +6,7 @@ import Base from "./Base"
 export default function Home() {
   console.log("API IS", API);
   return (
-    <Base title="Home Page">
+    <Base title="Home Page" description="Welcome to T-shirt store!!!!">
       <div className="row">
       <div className="col-4">
       <button className="btn btn-success">TEST</button>
