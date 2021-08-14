66745106084d944b773f7060fd49af76ce159bf5
theshifaligwal <gwal.shifali2223@gmail.com>
2021-08-14 13:28:43 +0530

    Testing signup route

--- src/user/Signup.js
+++ src/user/Signup.js
@@ -9,23 +9,23 @@ const Signup = () => {
     email: "",
     password: "",
     error: "",
-    success: false,
+    success: false
   });
 
   const { name, email, password, error, success } = values;
 
-  const handleChange = (name) => (event) => {
+  const handleChange = name => event => {
     setValues({ ...values, error: false, [name]: event.target.value });
   };
 
-  const onSubmit = (event) => {
+  const onSubmit = event => {
     event.preventDefault();
     setValues({ ...values, error: false });
     signup({ name, email, password })
-      .then((data) => {
-        if (data.error) {
+      .then(data => {
+        if(data.error) {
           setValues({ ...values, error: data.error, success: false });
-        }else{
+        } else {
           setValues({
             ...values,
             name: "",
@@ -33,7 +33,7 @@ const Signup = () => {
             password: "",
             error: "",
             success: true
-          })
+          });
         }
       })
       .catch(console.log("Error in signup"));
@@ -50,6 +50,7 @@ const Signup = () => {
                 className="form-control"
                 onChange={handleChange("name")}
                 type="text"
+                value={name}
               />
             </div>
             <div className="form-group">
@@ -57,19 +58,55 @@ const Signup = () => {
               <input
                 className="form-control"
                 onChange={handleChange("email")}
-                type="text"
+                type="email"
+                value={email}
               />
             </div>
+
             <div className="form-group">
               <label className="text-light">Password</label>
               <input
-                className="form-control"
                 onChange={handleChange("password")}
+                className="form-control"
                 type="password"
+                value={password}
               />
             </div>
-            <button className="btn btn-success btn-block">Submit</button>
+            <button onClick={onSubmit} className="btn btn-success btn-block">
+              Submit
+            </button>
           </form>
+        </div>
+      </div>
+    );
+  };
+
+  const successMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-success"
+            style={{ display: success ? "" : "none" }}
+          >
+            New account was created successfully. Please{" "}
+            <Link to="/signin">Login Here</Link>
+          </div>
+        </div>
+      </div>
+    );
+  };
+
+  const errorMessage = () => {
+    return (
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div
+            className="alert alert-danger"
+            style={{ display: error ? "" : "none" }}
+          >
+            {error}
+          </div>
         </div>
       </div>
     );
@@ -77,8 +114,10 @@ const Signup = () => {
 
   return (
     <Base title="Sign up page" description="A page for user to sign up!">
+      {successMessage()}
+      {errorMessage()}
       {signUpForm()}
-    </Base>
+        </Base>
   );
 };
 
