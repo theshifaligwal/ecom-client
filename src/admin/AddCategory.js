18e94b0eb4beca84514eb19e24f8fa6513961bbd
theshifaligwal <gwal.shifali2223@gmail.com>
2021-08-26 17:33:41 +0530

    Category request to backend

--- src/admin/AddCategory.js
+++ src/admin/AddCategory.js
@@ -1,15 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 import Base from "../core/Base";
+import { isAuthenticated } from "../auth/helper";
+import { Link } from "react-router-dom";
 
 const AddCategory = () => {
-  return ( 
-    <Base title="Create a category here" description="Add a new category for new t-shirts" className="container bg-info p-4">
-    
-    <div className="row bg-white rounded">
-    <div className="col-md-8 offset-md-2"></div>
+  const [name, setName] = useState("initialState");
+  const [error, setError] = useState(false);
+  const [success, setSuccess] = useState(false);
+
+  const { user, token } = isAuthenticated();
+
+  const goBack = () => (
+    <div className="mt-5">
+      <Link className="btn btn-sm btn-success mb-3" to="/admin/dashboard">
+        Admin Home
+      </Link>
     </div>
+  );
+
+  const myCategoryForm = () => (
+    <form>
+      <div className="form-group">
+        <p className="lead">Enter the category</p>
+        <input
+          type="text"
+          className="form-control my-3"
+          autoFocus
+          required
+          placeholder="For Ex. Summer"
+        />
+        <button className="btn btn-outline-info">Create Category</button>
+      </div>
+    </form>
+  );
+
+  return (
+    <Base
+      title="Create a category here"
+      description="Add a new category for new t-shirts"
+      className="container bg-info p-4"
+    >
+      <div className="row bg-white rounded">
+        <div className="col-md-8 offset-md-2">
+          {myCategoryForm()}, {goBack()}
+        </div>
+      </div>
     </Base>
-  )
+  );
 };
 
 export default AddCategory;
