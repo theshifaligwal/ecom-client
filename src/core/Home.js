05c77c4782aabbaf9cbb7245f56fcdb434d98171
theshifaligwal <gwal.shifali2223@gmail.com>
2021-09-01 13:37:02 +0530

    Bring everthing to home page

--- src/core/Home.js
+++ src/core/Home.js
@@ -1,22 +1,41 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../styles.css";
-import {API} from "../backend"
-import Base from "./Base"
-import Card from "./Card"
+import { API } from "../backend";
+import Base from "./Base";
+import Card from "./Card";
+import { getProduct } from "./helper/coreapicalls";
 
 export default function Home() {
-  
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(false);
+
+  const loadAllProduct = () => {
+    getProduct().then((data) => {
+      if (data.error) {
+        setError(data.error);
+      } else {
+        setProducts(data);
+      }
+    });
+  };
+
+  useEffect(() => {
+    loadAllProduct();
+  }, []);
+
   return (
     <Base title="Home Page" description="Welcome to T-shirt store!!!!">
       <div className="row text-center">
-      <div className="col-4">
-      <Card />
-      </div>
-      <div className="col-4">
-      <button className="btn btn-success">TEST</button></div>
-      <div className="col-4">
-      <button className="btn btn-success">TEST</button></div>
-    
+        <h1 className="text-white text-center">All the t-shirts</h1>
+        <div className="row">
+          {products.map((product, index) => {
+            return (
+              <div key={index} className="col-4 mb-4">
+                <Card />
+              </div>
+            );
+          })}
+        </div>
       </div>
     </Base>
   );
